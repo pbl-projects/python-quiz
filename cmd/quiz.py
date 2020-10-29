c1c5f8fcb67f21f21ff6796f73df028bea1d1140
gprok <gprok60@gmail.com>
2020-10-29 16:46:59 +0200

    Write score in csv file

--- cmd/quiz.py
+++ cmd/quiz.py
@@ -2,6 +2,7 @@ import html
 import json
 import random
 from urllib.request import Request, urlopen
+import csv
 
 class Question:
     def __init__(self):
@@ -13,7 +14,8 @@ class Question:
         self.category = ''
         self.suffled_options = []
 
-    def get_answer_string(self):
+    # Creates a string with all answers, correct and incorrect, shuffled and numbered.
+    def get_all_answers_as_string(self):
         options = [self.correct_answer] + self.incorrect_answers
         random.shuffle(options)
         self.suffled_options = options
@@ -25,7 +27,7 @@ class Question:
         return s
 
     def __str__(self):
-        return 'Question: ' + self.question + '\n' + self.get_answer_string()
+        return 'Question: ' + self.question + '\n' + self.get_all_answers_as_string()
 
     def ask(self):
         print(self)
@@ -53,7 +55,7 @@ class Quiz:
             q.type = element['type']
             q.difficulty = element['difficulty']
             q.question = html.unescape(element['question'])
-            q.correct_answer = element['correct_answer']
+            q.correct_answer = html.unescape(element['correct_answer'])
             for ans in element['incorrect_answers']:
                 q.incorrect_answers.append(html.unescape(ans))
             self.questions.append(q)
@@ -71,11 +73,18 @@ class Quiz:
         print("SCORE: " + str(self.score))
 
 
-# ask user for their name
+# 1. Play many times. After completing quiz, ask if user wants to play again
+# 2. Create new program to display statics. Username, Average Score, Times Played
+# 3. Display top 5 players
+
+name = input("Your name: ")
 quiz = Quiz()
 quiz.load()
 while quiz.has_more_questions():
     quiz.ask_current()
 quiz.print_score()
-# save user-name and score in file
+with open("score.csv", 'a') as score_file:
+    csv_file = csv.writer(score_file)
+    row = [name, quiz.score]
+    csv_file.writerow(row)
 print("Bye, Thanks for trying")
